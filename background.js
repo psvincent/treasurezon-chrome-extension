e55fd3e12756c52436644e0b80da4f722e20c169
psvincent12 <psvincent12@gmail.com>
2022-05-13 15:06:31 -0500

    Initial Add

---
+++ background.js
@@ -0,0 +1,8 @@
+function redirectToAffiliate() {
+    JSON.localStorage.getItem('redirects') || '[]';
+    return JSON.parse(json);
+}
+
+function setRedirectSites(list) {
+    localStorage.setItem
+}
---
+++ popup.js
@@ -0,0 +1,16 @@
+let changeColor = document.getElementById("changeColor");
+
+changeColor.addEventListener("click", async () => {
+    let[tab] = await chrome.tabs.query({ active: true, currentWindow: true});
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+    })
+})
+
+function setPageBackgroundColor() {
+chrome.storage.sync.get("color", ({ color }) => {
+    changeColor.style.backgroundColor = color;
+});
+}
